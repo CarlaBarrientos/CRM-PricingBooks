d63e787bf4f9655b2786cd7582d4cdd82e8572e2
Melisa Rojas <melisa.rs.1990@gmail.com>
2020-04-20 00:01:28 -0400

    Adding DTOModels with PricingBookDTO class and ProductPriceDTO class

---
+++ CRM-PricingBooks/Controllers/DTOModels/PricingBookDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM_PricingBooks.Controllers.DTOModels
+{
+    public class PricingBookDTO
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Status { get; set; }
+
+        public List<ProductPriceDTO> ProductPrices { get; set; }
+    }
+}
